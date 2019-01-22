1b44f3f27d0710df36be76a2eda0110a05c9a552
MichealYoungGo <ustb_yhb@163.com>
2019-01-22 21:06:14 +0800

    页码

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -20,4 +20,7 @@ class Blog(models.Model):
     def __str__(self):
         return "<Blog:%s>" % self.title
 
+    class Meta:
+        ordering = ['-created_time']
+
 
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render_to_response, get_object_or_404
+from django.core.paginator import Paginator
 from .models import Blog, BlogType
 
 def blog_list(request):
+
+    blogs_all_list = Blog.objects.all()
+    paginator = Paginator(blogs_all_list, 2) #每2页进行分页
+    page_num = request.GET.get('page', 1)  # 获取url的页面参数
+    page_of_blogs = paginator.get_page(page_num)
+
+
+
     context = {}
-    context['blogs'] = Blog.objects.all()
+    context['page_of_blogs'] = page_of_blogs
     context['blog_types'] = BlogType.objects.all()
     return render_to_response('blog/blog_list.html', context)
 
