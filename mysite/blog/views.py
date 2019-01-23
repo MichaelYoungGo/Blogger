fbb4adaf3885e216a51b3fce2efb507792f1f2d6
MichealYoungGo <ustb_yhb@163.com>
2019-01-23 21:32:53 +0800

    venv

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -69,3 +69,4 @@ def blog_detail(request, blog_pk):
     context['next_blog'] = Blog.objects.filter(created_time__lt=blog.created_time).first()
     context['blog'] = blog
     return render_to_response('blog/blog_detail.html', context)
+
