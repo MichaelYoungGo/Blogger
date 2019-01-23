051e726e6483e83753e66f7a57e49c40d33bb15a
MichealYoungGo <ustb_yhb@163.com>
2019-01-23 17:25:58 +0800

    view视图控制

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,25 +1,24 @@
 from django.shortcuts import render_to_response, get_object_or_404
 from django.core.paginator import Paginator
 from .models import Blog, BlogType
+from django.db.models import Count
 from django.conf import settings
 
 
-def blog_list(request):
-    blogs_all_list = Blog.objects.all()
-    paginator = Paginator(blogs_all_list, settings.BLOG_NUMBER_OF_EACH_PAGE)  # 每2页进行分页
-    page_num = request.GET.get('page', 1)  # 获取url的页面参数
+def get_blog_list_common_data(request, blogs_all_list):
+    paginator = Paginator(blogs_all_list, settings.BLOG_NUMBER_OF_EACH_PAGE)
+    page_num = request.GET.get('page', 1)  # 获取url的页面参数（GET请求）
     page_of_blogs = paginator.get_page(page_num)
-    currenter_page_num = page_of_blogs.number  # 获取当前页码
-    # 获取当前页码前后两页的页码范围
-    page_range = list(range(max(currenter_page_num - 2, 1), currenter_page_num)) + \
-                 list(range(currenter_page_num, min(currenter_page_num + 2, paginator.num_pages) + 1))
-    # 加上省略号
+    currentr_page_num = page_of_blogs.number  # 获取当前页码
+    # 获取当前页码前后各2页的页码范围
+    page_range = list(range(max(currentr_page_num - 2, 1), currentr_page_num)) + \
+                 list(range(currentr_page_num, min(currentr_page_num + 2, paginator.num_pages) + 1))
+    # 加上省略页码标记
     if page_range[0] - 1 >= 2:
         page_range.insert(0, '...')
     if paginator.num_pages - page_range[-1] >= 2:
         page_range.append('...')
-
-    # 加上首页和页尾
+    # 加上首页和尾页
     if page_range[0] != 1:
         page_range.insert(0, 1)
     if page_range[-1] != paginator.num_pages:
@@ -29,56 +28,30 @@ def blog_list(request):
     blog_dates = Blog.objects.dates('created_time', 'month', order="DESC")
     blog_dates_dict = {}
     for blog_date in blog_dates:
-        blog_count = Blog.objects.filter(created_time__year=blog_date.year, created_time__month=blog_date.month).count()
+        blog_count = Blog.objects.filter(created_time__year=blog_date.year,
+                                         created_time__month=blog_date.month).count()
         blog_dates_dict[blog_date] = blog_count
 
     context = {}
     context['blogs'] = page_of_blogs.object_list
     context['page_of_blogs'] = page_of_blogs
     context['page_range'] = page_range
-    context['blog_types'] = BlogType.objects.all()
+    context['blog_types'] = BlogType.objects.annotate(blog_count=Count('blog'))
     context['blog_dates'] = blog_dates_dict
+    return context
+
+
+def blog_list(request):
+    blogs_all_list = Blog.objects.all()
+    context = get_blog_list_common_data(request, blogs_all_list)
     return render_to_response('blog/blog_list.html', context)
-
-
-def blog_detail(request, blog_pk):
-    context = {}
-    blog = get_object_or_404(Blog, pk=blog_pk)
-    context = {}
-    context['previous_blog'] = Blog.objects.filter(created_time__gt=blog.created_time).last()
-    context['next_blog'] = Blog.objects.filter(created_time__lt=blog.created_time).first()
-    context['blog'] = blog
-    return render_to_response('blog/blog_detail.html', context)
 
 
 def blogs_with_type(request, blog_type_pk):
     blog_type = get_object_or_404(BlogType, pk=blog_type_pk)
     blogs_all_list = Blog.objects.filter(blog_type=blog_type)
-    paginator = Paginator(blogs_all_list, settings.BLOG_NUMBER_OF_EACH_PAGE)  # 每2页进行分页
-    page_num = request.GET.get('page', 1)  # 获取url的页面参数
-    page_of_blogs = paginator.get_page(page_num)
-    currenter_page_num = page_of_blogs.number  # 获取当前页码
-    # 获取当前页码前后两页的页码范围
-    page_range = list(range(max(currenter_page_num - 2, 1), currenter_page_num)) + \
-                 list(range(currenter_page_num, min(currenter_page_num + 2, paginator.num_pages) + 1))
-    # 加上省略号
-    if page_range[0] - 1 >= 2:
-        page_range.insert(0, '...')
-    if paginator.num_pages - page_range[-1] >= 2:
-        page_range.append('...')
-
-    # 加上首页和页尾
-    if page_range[0] != 1:
-        page_range.insert(0, 1)
-    if page_range[-1] != paginator.num_pages:
-        page_range.append(paginator.num_pages)
-
-    context = {}
+    context = get_blog_list_common_data(request, blogs_all_list)
     context['blog_type'] = blog_type
-    context['blogs'] = page_of_blogs.object_list
-    context['page_of_blogs'] = page_of_blogs
-    context['page_range'] = page_range
-    context['blog_types'] = BlogType.objects.all()
     return render_to_response('blog/blogs_with_type.html', context)
 
 
@@ -87,3 +60,12 @@ def blogs_with_date(request, year, month):
     context = get_blog_list_common_data(request, blogs_all_list)
     context['blogs_with_date'] = '%s年%s月' % (year, month)
     return render_to_response('blog/blogs_with_date.html', context)
+
+
+def blog_detail(request, blog_pk):
+    blog = get_object_or_404(Blog, pk=blog_pk)
+    context = {}
+    context['previous_blog'] = Blog.objects.filter(created_time__gt=blog.created_time).last()
+    context['next_blog'] = Blog.objects.filter(created_time__lt=blog.created_time).first()
+    context['blog'] = blog
+    return render_to_response('blog/blog_detail.html', context)
