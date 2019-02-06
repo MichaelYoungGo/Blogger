b0be5ed2b854d846d2f64284425f5a3f0260ac2e
MichealYoungGo <ustb_yhb@163.com>
2019-02-06 18:34:25 +0800

    网站图标

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,11 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.contrib.staticfiles.views import serve
 from . import views
 
 
 urlpatterns = [
     path('', views.home, name='home'),
+    #path('favicon.ico', serve, {'path': 'favicon.ico'}),
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
 ]
