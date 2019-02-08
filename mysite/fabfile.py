e4b30e0df5aa87db536d505ffe40dd16ba09059b
MichealYoungGo <ustb_yhb@163.com>
2019-02-08 11:12:56 +0800

    部署文件

---
+++ mysite/fabfile.py
@@ -0,0 +1,13 @@
+from fabric.api import env, run, cd
+
+env.hosts = ['47.92.135.212']
+env.user = 'root'
+env.password = '!Yang151750'
+
+
+def deploy():
+    with cd('/home/Blogger'):
+        run('ls')
+        with cd('/home/mysite_uwsgi'):
+            run('ls')
+#           run('uwsgi --reload master.pid')
